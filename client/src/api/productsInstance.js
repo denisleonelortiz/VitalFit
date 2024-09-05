e0c9ef80a6df702d77ccc5cddf7a732cd966a2c9
denisleonelortiz <denisleonelortiz@gmail.com>
2024-09-05 00:06:44 -0300

    deploy3

--- client/src/api/productsInstance.js
+++ client/src/api/productsInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const productsIns = axios.create({
-  baseURL: "https://vitalfit-production.up.railway.app/",
+  baseURL: "vitalfitback-production.up.railway.app",
   //baseURL: "http://localhost:3001",
 });
 // https://vitalfitapi.onrender.com
